789dbddef1336f01c8d453bc570bd98ea5b845e4
Daae-Kim <wlsemrdl1212@naver.com>
2025-09-16 12:22:01 +0900

    init commit

---
+++ src/main/java/com/practice/springresttemplateclient/service/RestTemplateService.java
@@ -0,0 +1,28 @@
+package com.practice.springresttemplateclient.service;
+
+import com.practice.springresttemplateclient.dto.ItemDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class RestTemplateService {
+
+    public ItemDto getCallObject(String query) {
+        return null;
+    }
+
+    public List<ItemDto> getCallList() {
+        return null;
+    }
+
+    public ItemDto postCall(String query) {
+        return null;
+    }
+
+    public List<ItemDto> exchangeCall(String token) {
+        return null;
+    }
+}
